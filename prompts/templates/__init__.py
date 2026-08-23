80c8074528c4b9caf214c83cf1e21807bbc01efe
nsdmlk <ilya.emelyanov2007@gmail.com>
2026-08-23 20:25:44 +0300

    clear repo

--- prompts/templates/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import PromptTemplate
-from .classification import ZERO_SHOT, FEW_SHOT
-from .summarization import BASIC, DETAILED
-from .extraction import ENTITIES, JSON
-from .qa import CONTEXT_BASED, REASONING
--- prompts/templates/base.py
+++
@@ -1,9 +0,0 @@
-class PromptTemplate:
-    def __init__(self, template: str):
-        self.template = template
-
-    def format(self, **kwargs) -> str:
-        return self.template.format(**kwargs)
-
-    def batch_format(self, batch_kwargs: list[dict]) -> list[str]:
-        return [self.format(**kwargs) for kwargs in batch_kwargs]
